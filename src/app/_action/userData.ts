57b522a8806b915693964b296790e06c3ed2a83b
imskanand <the.anand3099@gmail.com>
2023-12-11 13:02:06 +0530

    Log In function

--- src/app/_action/userData.ts
+++ src/app/_action/userData.ts
@@ -1,20 +1,24 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { db } from "~/server/db";
-
-export async function getUsers() {
-  const users = await db.user.findMany({
-    orderBy: {
-      username: "asc",
-    },
-  });
-  return users;
-}
 
 export async function addUser(formData: FormData) {
   const username = formData.get("username") as string;
   const email = formData.get("email") as string;
+
+  const userData = await db.user.findFirst({
+    where: {
+      email,
+      username,
+    },
+  });
+
+  if (userData) {
+    redirect(`/user/${userData?.id}`);
+  }
+
   const user = await db.user.create({
     data: {
       username,
--- src/app/user/AddUsers.tsx
+++ src/app/user/AddUsers.tsx
@@ -1,25 +1,23 @@
 import { addUser } from "~/app/_action/userData";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import { Label } from "~/components/ui/label";
 
 export default function AddUser() {
   return (
     <form action={addUser} className="flex max-w-3xl flex-col gap-3">
-      <label htmlFor="username">Username</label>
-      <input
-        type="text"
-        name="username"
-        id="username"
-        className="rounded-md p-2 text-xl outline"
-      />
-      <label htmlFor="email">Email</label>
-      <input
+      <Label htmlFor="username">Username</Label>
+      <Input type="text" name="username" id="username" placeholder="Username" />
+      <Label htmlFor="email">Email</Label>
+      <Input
         type="email"
         name="email"
         id="email"
-        className="rounded-md p-2 text-xl outline"
+        placeholder="Your Email ..."
       />
-      <button type="submit" className="rounded-lg bg-blue-500 p-3">
-        Add User
-      </button>
+      <Button type="submit" className="rounded-lg bg-blue-700 p-3 text-lg">
+        Sign In
+      </Button>
     </form>
   );
 }
